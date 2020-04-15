64ae9390a380fed56e779da1a4131d54c364d103
Greg Johnston <gcjohnston91@gmail.com>
2020-04-15 16:21:31 -0400

    This worked for all tests but 2. Trying not to do a nested loop. May though. Feeling wipsy.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,24 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    profit = 0
+    sell_price = prices[0]
+
+    for price in prices:
+        sell_price = min(sell_price, price)
+        profit_comparison = price - sell_price
+        profit = max(profit_comparison, profit)
+
+    return profit
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
--- stock_prices/test_stock_prices.py
+++ stock_prices/test_stock_prices.py
@@ -1,14 +1,42 @@
 import unittest
-from stock_prices import find_max_profit 
+from stock_prices import find_max_profit
+
 
 class Test(unittest.TestCase):
 
-  def test_find_max_profit(self):
-    self.assertEqual(find_max_profit([10, 7, 5, 8, 11, 9]), 6)
-    self.assertEqual(find_max_profit([100, 90, 80, 50, 20, 10]), -10)
-    self.assertEqual(find_max_profit([1050, 270, 1540, 3800, 2]), 3530)
-    self.assertEqual(find_max_profit([100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79]), 94)
-  
+    def test_find_max_profit(self):
+        self.assertEqual(find_max_profit([10, 7, 5, 8, 11, 9]), 6)
+        self.assertEqual(find_max_profit([100, 90, 80, 50, 20, 10]), -10)
+        self.assertEqual(find_max_profit([1050, 270, 1540, 3800, 2]), 3530)
+        self.assertEqual(find_max_profit([100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79]), 94)
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
+
+'''
+Loop through list and find highest number if index is not [-1]
+Loop through list before highest number to find lowest number
+If highest number index == [1], check next element
+For i in range of list: if i > i+1 and index(i+1) == -1:
+Buy price == i[-2] sell price == i[-1]
+'''
+
+'''
+profit = 0
+for i in list:
+profit = 
+'''
+
+'''
+All of the above is terrible
+profit = 0
+min_price = list[0]
+Start an list[0]
+For price in prices
+set min_price to whatever is smaller, price, or current index
+subtract min_price from price for profit_comparison
+A[0] = 5; A[1] = 10; A[2] = 20
+min_price = 5; price = 10; profit_comparison = 5
+set profit to whatever is higher between current profit and profit_comparison 
+'''
