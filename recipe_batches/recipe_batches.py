326cdd1b91e712d21439e971f4bafe04b1a5bd60
Greg Johnston <gcjohnston91@gmail.com>
2020-04-16 12:53:00 -0400

    Working on recipe batch

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,26 @@
 
 import math
 
+recipe_test = {'milk': 100, 'butter': 50, 'cheese': 10}
+ingredients_test = {'milk': 198, 'butter': 52, 'cheese': 10}
+
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    recipe_item = list(recipe.keys())
+    ingredients_items = list(ingredients.keys())
+    # smallest_possible = ingredients_items[0] // recipe_item[0]
+    print(recipe[recipe_item[0]])
+    # print(recipe_item)
+    # print(ingredients_items)
+    if len(recipe) != len(ingredients):
+        return 0
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+recipe_batches(recipe_test, ingredients_test)
+# if __name__ == '__main__':
+#     # Change the entries of these dictionaries to test
+#     # your implementation with different inputs
+#     recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+#     ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+#     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+#         batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
