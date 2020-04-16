993ab0d59a3cbd7b8bef6d7bc8988cb18b8ba6cc
Greg Johnston <gcjohnston91@gmail.com>
2020-04-16 12:04:26 -0400

    Stock Prices done

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,22 +4,44 @@ import argparse
 
 
 def find_max_profit(prices):
-    profit = 0
-    sell_price = prices[0]
+    current_min_price = prices[0]
+    print(current_min_price)
+    max_profit = prices[1] - current_min_price
+    print(max_profit)
     n = len(prices)
 
-    for price in prices:
-        sell_price = min(sell_price, price)
-        profit_comparison = price - sell_price
-        profit = max(profit_comparison, profit)
-    return profit
+    for i in range(n):
+        for j in range(i + 1, n):
+            print(prices[j], prices[i], max_profit)
+            if max_profit < prices[j] - prices[i]:
+                max_profit = prices[j] - prices[i]
+    return max_profit
 
-    # gunna try to brute force it
-    # I'm sorry Big O
-    # I have failed you
 
-    # for i in range(prices):
+#     max_profit = float('-inf')
+#     n = len(prices)
+#     for i in prices:
+#         for j in range(i + 1, n):
+#             if i < prices[j]:
+#                 temp_profit = prices[j] - i
+#                 if temp_profit > max_profit:
+#                     max_profit = temp_profit
+#             else:
+#                 temp_profit = prices[j] - i
+#                 if temp_profit > max_profit:
+#                     max_profit = temp_profit
+#     return max_profit
 
+
+'''
+10 20
+Instantiate max-profit variable = -infinity
+Iterate through all the values in our input, i
+Iterate through all subsequent values, not including the first / current value in list, j
+If difference between j and i is greater than current max profit, then
+Set max_profit equal to what we find from this difference between j and i
+Return max-profit value
+'''
 
 if __name__ == '__main__':
     # This is just some code to accept inputs from the command line
